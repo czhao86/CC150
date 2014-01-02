59224bb3fc141ec4e0d975c083a5fae6526132a0
czhao86 <czhao86@gmail.com>
2014-01-02 11:52:22 -0500

    Create 3.5.cpp

---
+++ 3.5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class myQueue
+{
+	stack<int> s1;
+	stack<int> s2;
+public:
+	myQueue(){}
+	~myQueue(){}
+	void qadd(int num)
+	{
+		s1.push(num);
+	}
+	int qpop()
+	{
+		while (!s1.empty())
+		{
+			s2.push(s1.top());
+			s1.pop();
+		}
+		return s2.top();
+	}
+};
+
+int main()
+{
+	myQueue s;
+	for (int i = 0; i < 10; ++i)
+	{
+		s.qadd(i);
+	}
+	cout << s.qpop() << endl;
+}
