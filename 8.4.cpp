32aa0852a5e0be72559338408fb887380e31870e
czhao86 <czhao86@gmail.com>
2014-01-13 15:27:38 -0500

    Update 8.4.cpp

--- 8.4.cpp
+++ 8.4.cpp
@@ -26,10 +26,31 @@ vector<string> allper(string a)
 	return result;
 }
 
+vector<string> allper1(string a)
+{
+	vector<string> result;
+	if (a == "")
+	{
+		result.push_back("");
+		return result;
+	}
+	for (int i = 0; i < a.length(); ++i)
+	{		
+		string one = a.substr(i, 1);
+		string aa = a;
+		vector<string> rest = allper1(aa.erase(i, 1));
+		for (int j = 0; j < rest.size(); ++j)
+		{
+			result.push_back(one + rest[j]);
+		}
+	}
+	return result;
+}
+
 int main()
 {
 	string aa = "abcde";
-	vector<string> bb = allper(aa);
+	vector<string> bb = allper1(aa);
 	for (int i = 0; i < bb.size(); ++i)
 		cout << bb[i] << endl;
 	return 0;
