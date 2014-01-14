20946cc144442aa4ee9bbb3c9198714b5bac56bf
czhao86 <czhao86@gmail.com>
2014-01-14 00:06:46 -0500

    Update 8.7.cpp

--- 8.7.cpp
+++ 8.7.cpp
@@ -24,7 +24,22 @@ int makeChange(int n, int denom)
 	return ways;
 }
 
+int makeChange1(int sum, int c, int n)
+{
+	int ways = 0;
+	if (sum <= n)
+	{
+		if (sum == n) return 1;
+		if (c >= 25) ways += makeChange1(sum + 25, 25, n);
+		if (c >= 10) ways += makeChange1(sum + 10, 10, n);
+		if (c >= 5) ways += makeChange1(sum + 5, 5, n);
+		if (c >= 1) ways += makeChange1(sum + 1, 1, n);
+	}
+	return ways;
+}
+
 int main()
 {
-	cout << makeChange(5, 5) << endl;
+	cout << makeChange(100, 25) << endl;
+	cout << makeChange1(0, 25, 100) << endl;
 }
