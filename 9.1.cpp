710c8a0d630d6b31f8c58a832bc4d18b493759df
Chao Zhao <czhao86@users.noreply.github.com>
2014-09-27 14:29:09 -0400

    Update 9.1.cpp

--- 9.1.cpp
+++ 9.1.cpp
@@ -3,30 +3,25 @@ using namespace std;
 
 void merge(int a[], int b[])
 {
-	int m = 4, n = 4, t = 9;
-	for (int i = 0; i < 10; ++i)
+	int m = 4, n = 4;
+	int i;
+	for (i = 9; i >=0; --i)
 	{
-		if (n >= 0)
+		if (n >= 0 && m>=0)
 		{
-			if (a[m]>b[n])
-				a[t--] = a[m--];
-			else a[t--] = b[n--];
-		}
-		if (m >= 0)
-		{
-			a[t--] = a[m--];
+			if (a[m]>b[n]) {a[i]=a[m];m--;}
+			else {a[i]=b[n];n--;}
 		}
 	}
+	while (n>=0)
+	{a[i]=b[n];n--;i--;}
+	while (m>=0)
+	{a[i]=a[m];m--;i--;}
 }
 
 int main()
 {
 	int a[10] = { 1, 3, 5, 7, 9 };
-	for (int i = 0; i < 10; ++i)
-	{
-		cout << a[i]<<' ';
-	}
-	cout << endl;
 	int b[5] = { 2, 4, 6, 8, 10 };
 	merge(a, b);
 	for (int i = 0; i < 10; ++i)
